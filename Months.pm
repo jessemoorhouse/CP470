671e2d0df488efa5791f95ecb036d60ce34186ce
jessemoorhouse <jmoorhouse@outlook.com>
2015-01-12 21:37:16 -0500

    updated Months.pm

--- Months.pm
+++ Months.pm
@@ -1,34 +1,40 @@
+
 package Months;
 sub new
 {
    my $class = shift;
    my $numberOfDays = {
-        _january => shift,
-	_february => shift,
-	_march => shift,
-	_april => shift,
-	_may => shift,
-	_june => shift,
-	_july => shift,
-	_august => shift,
-	_september => shift,
-	_october => shift,
-	_november => shift,
-	_december => shift,
+       _month => shift,
+       _number => shift,
    };
-#Prints out the number of days of each month in the year
-print "January: $numberofDays->{_january}\n";
-print "February: $numberofDays->{_february}\n";
-print "March: $numberofDays->{_march}\n";
-print "April: $numberofDays->{_april}\n";
-print "May: $numberofDays->{_may}\n";
-print "June: $numberofDays->{_june}\n";
-print "July: $numberofDays->{_july}\n";
-print "August: $numberofDays->{_august}\n";
-print "September: $numberofDays->{_september}\n";
-print "October: $numberofDays->{_october}\n";
-print "November: $numberofDays->{_november}\n";
-print "December: $numberofDays->{_december}\n";
-bless $numberofDays, $class;
-return $numberofDays;
+
+#Print out the number of days for each month
+print "Month:$numberOfDays->{_month} Number of Days:$numberOfDays->{_number}\n";
+bless $numberOfDays, $class;
+return $numberOfDays;
 }
+
+sub setNumberofDays{
+    my ($numberOfDays, $number) = @_;
+    $number->{_number} = $number if defined($number);
+    return $numberOfDays->{_number}; 
+}
+
+sub getNumberofDays{
+    my($numberOfDays) = @_;
+    return $numberOfDays->{_number};
+}
+
+sub setMonth{
+    my ($numberOfDays, $month) = @_;
+    $number->{_month} = $month if defined($month);
+    return $numberOfDays->{_month};
+ 
+}
+
+sub getMonth{
+    my($numberOfDays) = @_;
+    return $numberOfDays->{_month};
+}
+
+1;
